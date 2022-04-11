7623427dbbcfcfe2415896833ff80ce4895b0926
Sarthak Makhija <sarthak.makhija@gmail.com>
2022-04-11 13:39:39 +0530

    Sarthak | Adds dependencies

--- .github/sample.sh
+++ .github/sample.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-sudo apt install autoconf automake pkg-config libglib2.0-dev libfabric-dev pandoc libncurses5-dev valgrind
+sudo apt install autoconf automake pkg-config libglib2.0-dev libfabric-dev pandoc \
+libncurses5-dev valgrind libpmemobj-cpp-dev libdaxctl-dev \
+libndctl-dev libnuma-dev libtbb-dev libtool rapidjson-dev
 
 echo "##########################################"
