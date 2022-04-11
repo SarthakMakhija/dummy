e1dcd733b1bd1224f2168efd8602b61dd11fb340
Sarthak Makhija <sarthak.makhija@gmail.com>
2022-04-11 13:29:55 +0530

    Sarthak | Adds valgrind

--- .github/sample.sh
+++ .github/sample.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sudo apt install autoconf automake pkg-config libglib2.0-dev libfabric-dev pandoc libncurses5-dev
+sudo apt install autoconf automake pkg-config libglib2.0-dev libfabric-dev pandoc libncurses5-dev valgrind
 
 echo "##########################################"
