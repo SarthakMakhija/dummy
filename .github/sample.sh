e18aaa30061d8831742967e0cdce7016f54e71ec
Sarthak Makhija <sarthak.makhija@gmail.com>
2022-04-13 13:06:48 +0530

    Sarthak | Removes pmdk from cmake

--- .github/sample.sh
+++ .github/sample.sh
@@ -5,9 +5,9 @@ libndctl-dev libnuma-dev libtbb-dev libtool rapidjson-dev pandoc valgrind
 
 #libpmemobj-cpp-dev
 
-#cd ~
-#git clone https://github.com/pmem/pmdk
-#cd pmdk
-#make -j$(nproc)
-#sudo make install
+cd ~
+git clone https://github.com/pmem/pmdk
+cd pmdk
+make -j$(nproc)
+sudo make install
 
