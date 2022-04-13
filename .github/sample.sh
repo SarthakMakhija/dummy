fefbf376ca812ce77492c3d6848367b49d949cdc
Sarthak Makhija <sarthak.makhija@gmail.com>
2022-04-13 12:52:51 +0530

    Sarthak | Installs libpmemobj using cmake

--- .github/sample.sh
+++ .github/sample.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 sudo apt install autoconf automake build-essential libdaxctl-dev \
-libndctl-dev libnuma-dev libtbb-dev libtool rapidjson-dev pandoc valgrind libpmemobj-cpp-dev
+libndctl-dev libnuma-dev libtbb-dev libtool rapidjson-dev pandoc valgrind
+
+#libpmemobj-cpp-dev
 
 #cd ~
 #git clone https://github.com/pmem/pmdk
