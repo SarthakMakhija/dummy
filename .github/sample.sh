adf5d6c1c43f279de621efea5f12360c330d77ac
Sarthak Makhija <sarthak.makhija@gmail.com>
2022-04-13 11:37:39 +0530

    Sarthak | Adds pmdk in CMake

--- .github/sample.sh
+++ .github/sample.sh
@@ -3,9 +3,9 @@
 sudo apt install autoconf automake build-essential cmake libdaxctl-dev \
 libndctl-dev libnuma-dev libtbb-dev libtool rapidjson-dev pandoc valgrind libpmemobj-cpp-dev
 
-cd ~
-git clone https://github.com/pmem/pmdk
-cd pmdk
-make -j$(nproc)
-sudo make install
+#cd ~
+#git clone https://github.com/pmem/pmdk
+#cd pmdk
+#make -j$(nproc)
+#sudo make install
 
