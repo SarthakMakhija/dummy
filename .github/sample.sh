3c0bfe9c5df83cabdc494687457f7d7ebe3103e2
Sarthak Makhija <sarthak.makhija@gmail.com>
2022-04-13 15:31:02 +0530

    Sarthak | lists files in /usr/local/lib/

--- .github/sample.sh
+++ .github/sample.sh
@@ -2,6 +2,8 @@
 
 sudo rm -rf /usr/local/lib/libpmem*
 
+ls -ltr /usr/local/lib/libpmem*
+
 sudo apt install autoconf automake build-essential libdaxctl-dev \
 libndctl-dev libnuma-dev libtbb-dev libtool rapidjson-dev pandoc valgrind
 
